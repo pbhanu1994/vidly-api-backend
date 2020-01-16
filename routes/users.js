c9c3d2a8c973c0cd053339b024468ca110c037e9
Bhanu <pbhanu.1994@gmail.com>
2020-01-16 13:43:42 +1000

    Added access-control-expose-headers for x-auth-token

--- routes/users.js
+++ routes/users.js
@@ -37,7 +37,10 @@ router.post('/', async (req, res) => {
     const token = user.generateAuthToken();
 
     // Sendint the token in the header, and other properties in normal response
-    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
+    res
+        .header('x-auth-token', token)
+        .header("access-control-expose-headers", "x-auth-token")
+        .send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
